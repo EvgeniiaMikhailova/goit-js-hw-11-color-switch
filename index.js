cd0c4b5afdf0d6f8e4be3976fcf73f36769bb88d
EvgeniiaMikhailova <evgeva99@gmail.com>
2021-08-19 23:13:20 +0300

    Добавляет скрипт

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const colors = [
     '#795548',
 ];
 
+let intervalId = null;
+let isActive = false;
+
+const btnStart = document.getElementById('btnStart');
+const btnStop = document.getElementById('btnStop');
+const body = document.querySelector('body');
+
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const onBtnStart = () => {
+    if (isActive) {
+        console.log('изменение темы запущено, кнопка старт не активна');
+        return;
+    }
+    isActive = true;
+
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+        console.log('изменяю цвет фона');
+    }, 1000);
+
+    btnStart.disabled = true;
+};
+
+btnStart.addEventListener('click', onBtnStart);
+
+const onBtnStop = () => {
+   clearInterval(intervalId, console.log('останавливаю изменения цвета фона'));
+  isActive = false;
+    
+    btnStart.disabled = false;
+};
+
+btnStop.addEventListener('click', onBtnStop);
